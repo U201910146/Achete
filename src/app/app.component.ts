619097fd195030228ec8a9b440fc5ec42aa0aff2
Tomas Ortiz <u201910146@upc.edu.pe>
2024-06-21 00:24:48 -0500

    feat: Initial Toolbar Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import {CustomToolbarComponent} from "./Public/components/custom-toolbar/custom-toolbar.component";
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, CustomToolbarComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'achete-frontend';
+  title = 'Achete-Frontend';
 }
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,5 +1,6 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
+import { CustomToolbarComponent } from './Public/components/custom-toolbar/custom-toolbar.component'
 
 import { routes } from './app.routes';
 
